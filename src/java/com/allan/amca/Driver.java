d6a2d8b9cbc046e8a84eabbe445d6880479f0f0e
SwiftDBMSLamas <67307011+SwiftDBMSLamas@users.noreply.github.com>
2021-07-22 19:57:23 -0700

    merge

--- src/java/com/allan/amca/Driver.java
+++ src/java/com/allan/amca/Driver.java
@@ -1,5 +1,6 @@
 package com.allan.amca;
 
+<<<<<<< HEAD
 import com.allan.amca.data.DatabaseHelper;
 import com.allan.amca.gui.Screen;
 import com.allan.amca.login.Login;
@@ -7,10 +8,16 @@ import com.allan.amca.user.UserFactory;
 import com.allan.amca.user.UserFactoryGenerator;
 
 import javax.swing.*;
+=======
+import Data.DatabaseHelper;
+import com.allan.amca.user.UserFactory;
+import com.allan.amca.user.UserFactoryGenerator;
+>>>>>>> master
 
 public class Driver {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
         DatabaseHelper dbHelper = DatabaseHelper.getInstance();
         final UserFactory userFactory = new UserFactoryGenerator();
         final Login login = Login.getInstance();
@@ -19,5 +26,16 @@ public class Driver {
         screen.Mainframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         screen.Mainframe.setSize(400, 280);
         screen.Mainframe.setVisible( true );
+=======
+        DatabaseHelper dbHelper = new DatabaseHelper();
+        final UserFactory userFactory = new UserFactoryGenerator();
+        final Login login = Login.getInstance();
+//        final Personable user = userFactory.CreateUser(UserType.USER, "Junho", "Lee", "testing1pass");
+//        final Personable user2 = userFactory.CreateUser(UserType.USER, "Minjun", "Kim", "testing2pass");
+//        System.out.println(user.getClientID());
+//        System.out.println(user2.getClientID());
+//        dbHelper.addClient(user2);
+        login.login("4519011123012301", "testing1pass");
+>>>>>>> master
     }
 }
